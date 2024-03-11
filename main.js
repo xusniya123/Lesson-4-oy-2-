e0628e7d4b3c3fb7cf43202e31fc4e52bf96745b
xusniya123 <xusniya12345678@gmail.com>
2024-03-11 13:27:34 +0500

    Lesson 4 moth (2)

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict"
+let stiring  =prompt("String typedagi ma'lumot")
+
+
+let Number =prompt("Number typedagi ma'lumot")
+
+
+let boolen =prompt("Boolen typedagi ma'lumot")
+
+
+alert("Mening ismim =  " +  stiring +  " \n  Mening familyam  = " +  Number  +  "\n Mening yoshim ="+ boolen )
